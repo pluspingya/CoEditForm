7b9ffc9b45eb1c78ee8f7f9a204dcb444502c9a4
Plus Pingya <pluspingya@gmail.com>
2019-11-08 23:14:03 +1100

    remove unused codes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,24 +1,14 @@
-// import axios from 'axios'
-
 import {
     FORM_LOAD,
     FORM_UPDATE,
 } from './types'
 
-// export const loadForm = () => async dispatch => {
-//     const res = await axios.get('/api/loadForm')
-//     dispatch({type: FORM_LOAD, payload: res.data})
-// }
-
 export const loadFormWS = (client) => async dispatch => {
-    client.action('loadForm', {}, data => dispatch({type: FORM_LOAD, payload: data}))
+    client.action('loadForm', {}, data =>  {
+        console.log(data)
+        dispatch({type: FORM_LOAD, payload: data})
+    })
 }
-
-// export const updateForm = ({title, type, description, tags}) => async dispatch => {
-//     const params = {title, type, description, tags}
-//     const res = await axios.post('/api/updateForm', params)
-//     dispatch({type: FORM_UPDATE, payload: res.data})
-// }
 
 export const updateFormWS = (client, {title, type, description, tags}) => async dispatch => {
     const params = {title, type, description, tags}
@@ -28,7 +18,6 @@ export const updateFormWS = (client, {title, type, description, tags}) => async 
 }
 
 export const storeDispatch = data => async dispatch => {
-    console.log(data)
-    const payload = {formData: data.message}
-    dispatch({type: FORM_UPDATE, payload})
+    console.log(data.message)
+    dispatch({type: FORM_UPDATE, payload: data.message})
 }
