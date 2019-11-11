a7ef36aed84f606fc29e32a360bea7c94faa2e61
Plus Pingya <pluspingya@gmail.com>
2019-11-11 22:37:01 +1100

    make react starts once window.onload

--- client/src/index.js
+++ client/src/index.js
@@ -11,18 +11,15 @@ import reduxThunk from 'redux-thunk'
 import Form from './components/Form'
 import reducers from './reducers'
 
-import * as serviceWorker from './serviceWorker';
+window.onload = () => {
+    console.log('start')
+    const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+    ReactDOM.render(
+        <Provider store={store}>
+            <Form />
+        </Provider>, 
+        document.getElementById('root')
+    )
+}
 
-ReactDOM.render(
-    <Provider store={store}>
-        <Form />
-    </Provider>, 
-    document.getElementById('root')
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
